990584388126ac1060ed2e35a72e4c7c77d4f9b1
Arya756 <ayusharyan1709@gmail.com>
2025-12-04 18:30:06 +0530

    Remove CORS and update CORS configuration

--- server/server.js
+++ server/server.js
@@ -1,5 +1,4 @@
 const express = require('express');
-const cors = require('cors');
 const dotenv = require('dotenv');
 const connectDB = require('./config/database');
 
@@ -8,7 +7,6 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT || 5001;
 
-app.use(cors());
 app.use(express.json());
 
 // Routes
